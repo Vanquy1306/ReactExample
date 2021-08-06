b3e96544e01e05e060ed5bb77290ce1f6b675603
vanquy <phamvanquy1306@gmail.com>
2021-08-06 13:37:26 +0700

    v1

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,6 +1,6 @@
 export const apiUrl =
 	process.env.NODE_ENV !== 'production'
-		? 'http://localhost:5000/api':''
+		? 'http://localhost:5000/api':'https://polar-brook-85659.herokuapp.com/api'
 
         export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern'
         export const POSTS_LOADED_SUCCESS = 'POSTS_LOADED_SUCCESS'
